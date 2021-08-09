d01639ec11a4d677389189ee0d3d360fad7a30c8
kenmac999 <86565623+kenmac999@users.noreply.github.com>
2021-08-08 21:14:02 -0500

    Add files via upload
    Update with comments to explain how to use shell script

--- timestamp.sh
+++ timestamp.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+#----------------------------------------------------------------------------#
+# File: timeestamp.sh
+# Last Update: Sun 08 Aug 2021 09:04:57 PM CDT 
+# Purpose: inserts date, time before any text passed to it.
+# Example:
+# echo "This is a test" 2>&1 | /home/dad/Bash_Scripts/timestamp.sh 
+#----------------------------------------------------------------------------#
 while read x; do
 	echo -n `date +%Y-%m-%d\ %H:%M:%S`;
-	echo -n " ";
+	echo -n " - ";
 	echo $x;
 done
